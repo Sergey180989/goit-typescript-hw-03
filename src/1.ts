62a3743e0257bbd8af4728f0a5ec7d282d0af1ca
Sergey180989 <133609830+Sergey180989@users.noreply.github.com>
2024-01-08 22:25:52 +0200

    change-1ts

--- src/1.ts
+++ src/1.ts
@@ -19,11 +19,7 @@
 // export {};
 
 class Student {
-  constructor(public name: string, public age: number, public grade: string) {
-    this.name = name;
-    this.age = age;
-    this.grade = grade;
-  }
+  constructor(public name: string, public age: number, public grade: string) {}
 }
 
 export {};
